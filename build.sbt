5c5b838fb9527d1ee7bcf44ca58bcc0c764f6cb1
andrew <andrew.a846@gmail.com>
2018-10-10 16:29:07 +0300

    feat: add mid in alerting push

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ organization := "im.dlg"
 
 name := "dialog-push-service"
 
-version := "0.2.0.3"
+version := "0.2.0.4"
 
 scalaVersion := "2.11.11"
 
--- proto-gen.sh
+++ proto-gen.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 #protoc -I src/protobuf/ src/protobuf/push_service.proto --go_out=plugins=grpc:src/server
-protoc -I src/protobuf/ src/protobuf/push_service.proto --gogoslick_out=plugins=grpc:src/server
+protoc -I src/protobuf/ \
+--gogoslick_out=Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,\
+plugins=grpc:src/server \
+src/protobuf/push_service.proto
+
